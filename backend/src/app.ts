689a9ec5163f1ff3dd8f7e7a55f34ec0ccbeb1fb
Vaibhav <vaibhavsinghvrajpot@gmail.com>
2025-06-02 22:21:15 +0530

    project setup with frontend and backend

---
+++ backend/src/app.ts
@@ -0,0 +1,12 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+
+
+
+const app: Application = express();
+
+
+app.use("/", (req: Request, res: Response, next: NextFunction): void => {
+  res.json({ message: "Allo! Catch-all route." });
+});
+
+export default app;
